6d831b47f43c52c18eb8c174cc28ef644e13721c
ScottPeckover <scottpeckover@gmail.com>
2017-09-03 14:42:03 +0930

    animation fixes

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -29,17 +29,21 @@ public class Player_Controller : MonoBehaviour {
         else if (rb2d.velocity.x < 0)
             spriteRenderer.flipX = false;
         else animator.SetInteger("movement_state", 0);
-    }
 
-    void FixedUpdate ()
-    {
+        animator.SetFloat("movement_speed", (Mathf.Abs(rb2d.velocity.x) + maxSpeed) / maxSpeed);
         //Limits speed of player
         if (!(Mathf.Abs(rb2d.velocity.x) > maxSpeed))
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             Vector2 movement = new Vector2(moveHorizontal, 0);
-            rb2d.AddForce(movement * acceleration);
+            //rb2d.AddForce(movement * acceleration);
+            rb2d.velocity = new Vector2((moveHorizontal * acceleration) + rb2d.velocity.x, rb2d.velocity.y);
         }
+    }
+
+    void FixedUpdate ()
+    {
+        
         
     }
 
@@ -47,5 +51,7 @@ public class Player_Controller : MonoBehaviour {
     {
         GUI.Label(new Rect(10, 10, 200, 20), "Player x_velocity:");
         GUI.Label(new Rect(10, 30, 100, 20), rb2d.velocity.x + "");
+        GUI.Label(new Rect(10, 50, 200, 20), "Player y_velocity:");
+        GUI.Label(new Rect(10, 70, 100, 20), rb2d.velocity.y + "");
     }
 }
