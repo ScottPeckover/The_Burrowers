3d4fbefd9b7c9f4bd67272681717a18bf6ea330f
nawaz-shakir <nawaz.retailogics@gmail.com>
2017-09-19 20:53:31 +0930

    Vertical Movement Added

---
+++ Assets/Scripts/Hanging_Spider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hanging_Spider : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+	private float direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb2d.AddForce (new Vector2 (0f, direction));
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		direction = direction*-1;
+
+		if( coll.gameObject.tag == "Player" ){
+			Physics2D.IgnoreCollision(coll.collider, coll.otherCollider);
+			Debug.Log ("AAAAAAHHHH!!");
+			this.gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+	}
+
+}
