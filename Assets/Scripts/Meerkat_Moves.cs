5919904e3ccf38b4fa51f6f746d2227c5bd9d5aa
alth0042 <torki9022@gmail.com>
2017-09-19 23:08:16 +0930

    Meerkat Replaced

--- Assets/Scripts/Meerkat_Moves.cs
+++ Assets/Scripts/Meerkat_Moves.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Meerkat_Moves : MonoBehaviour {
 
 	public Rigidbody2D rb2d;
+	private SpriteRenderer spriteRenderer;
+	private float speed;
 
 	// Use this for initialization
 	void Start () {
-		rb2d.AddForce (new Vector2(1f, 0f));
-		rb2d.velocity = new Vector2 (1f, 0f);
+		speed = 0.5f;
+		rb2d.AddForce (new Vector2(speed, 0f));
+		rb2d.velocity = new Vector2 (speed, 0f);
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (rb2d.velocity.x > 0)
+			spriteRenderer.flipX = true;
+		else if (rb2d.velocity.x < 0)
+			spriteRenderer.flipX = false;
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
