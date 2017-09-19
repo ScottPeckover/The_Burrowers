2c872ea9d785d1471623b4c6c2c09765cb1fe3be
alth0042 <torki9022@gmail.com>
2017-09-19 21:57:24 +0930

    Meerkat Moves Added

---
+++ Assets/Scripts/Meerkat_Moves.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meerkat_Moves : MonoBehaviour {
+
+	public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d.AddForce (new Vector2(1f, 0f));
+		rb2d.velocity = new Vector2 (1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag == "Player") {
+			Physics2D.IgnoreCollision (col.collider, col.otherCollider);
+		}
+	}
+}
