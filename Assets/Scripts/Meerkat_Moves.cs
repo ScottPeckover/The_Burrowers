735de4e962c2152dd49f99fb9ecfdd6185c7c472
tomm177 <tom.bradleyy@gmail.com>
2017-10-20 11:31:09 +1030

    meerkat changes

--- Assets/Scripts/Meerkat_Moves.cs
+++ Assets/Scripts/Meerkat_Moves.cs
@@ -1,34 +1,36 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Meerkat_Moves : MonoBehaviour {
+public class Meerkat_Moves : Enemy {
 
-	public Rigidbody2D rb2d;
-	public float damage;
-	private SpriteRenderer spriteRenderer;
-	private float speed;
+
+	public Meerkat_Moves() {
+		health = 7.0f;
+		damage = 1.5f;
+		height = 110;
+
+		name = "Meerkat";
+	}
 
 	// Use this for initialization
 	void Start () {
-		damage = 1.5f;
-		speed = 0.5f;
-		rb2d.AddForce (new Vector2(speed, 0f));
-		rb2d.velocity = new Vector2 (speed, 0f);
-		spriteRenderer = GetComponent<SpriteRenderer>();
+//		damage = 1.5f;
+//		speed = 0.5f;
+		rb2d.velocity = new Vector2 (getSpeed()*getDirection(), 0f);
+		setSpriteRenderer (GetComponent<SpriteRenderer> ());
+//		height = (int) gameObject.GetComponent<BoxCollider2D>().size.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (rb2d.velocity.x > 0)
-			spriteRenderer.flipX = true;
-		else if (rb2d.velocity.x < 0)
-			spriteRenderer.flipX = false;
-	}
-
-	void OnCollisionEnter2D (Collision2D col) {
-		if (col.gameObject.tag == "Player") {
-			Physics2D.IgnoreCollision (col.collider, col.otherCollider);
+		if (!isPaused()) {
+//			rb2d.velocity = new Vector2 (getSpeed()*getDirection()*rb2d.velocity.x, 0f);
+			rb2d.AddForce (new Vector2 (getSpeed () * getDirection (), 0f));
+			flipX ((rb2d.velocity.x > 0.2));
+			flash ();
+		} else {
+			rb2d.velocity = new Vector2 (0f, 0f);
 		}
 	}
+
 }
