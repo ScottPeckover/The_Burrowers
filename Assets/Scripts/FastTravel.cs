2379844e4882c4339149c55f025ac35cd065c2da
tomm177 <tom.bradleyy@gmail.com>
2017-10-25 16:45:40 +1030

    scripts

---
+++ Assets/Scripts/FastTravel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FastTravel : MonoBehaviour {
+
+	LevelManager gm;
+	public int levelToLoad;
+	public Button levelSelectBtn;
+
+	public Image black;
+	public Animator anim;
+
+
+	void Start () {
+		gm = FindObjectOfType<LevelManager> ();
+		Button btn = levelSelectBtn.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		//Right Way
+		StartCoroutine(Fading());
+		//or
+		//StartCoroutine("Fading");
+
+		//Wont work
+		//Fading();
+	}
+
+	IEnumerator Fading() {
+		anim.SetBool ("Fade", true);
+		yield return new WaitUntil (() => black.color.a == 1);
+		SceneManager.LoadScene (levelToLoad);
+	}
+
+	void TaskOnClick() {
+		StartCoroutine (Fading ());
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	//Loads the specified scene
+	public void LoadNextLevel(int x) {
+		SceneManager.LoadScene (x);
+	}
+
+}
