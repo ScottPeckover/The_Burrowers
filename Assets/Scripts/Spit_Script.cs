a08348f826d9cf16f113232c1adb03901c575f9f
nawaz-shakir <nawaz.retailogics@gmail.com>
2017-10-28 22:50:31 +1030

    Spitting Spider Enemy Added

---
+++ Assets/Scripts/Spit_Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spit_Script : MonoBehaviour {
+
+	public Transform target;
+	private Vector2 direction;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		// direction = target.position - transform.position;
+		rb2d = GetComponent<Rigidbody2D> ();
+		// rb2d.AddForce (direction);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// rb2d.AddForce (direction);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if( col.gameObject.layer == LayerMask.NameToLayer("Ground") || col.gameObject.layer == LayerMask.NameToLayer("Player") )
+			Destroy (col.otherCollider.gameObject);
+	}
+}
---
+++ Assets/Scripts/Spitting_Spider_Script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spitting_Spider_Script : Enemy {
+
+	public GameObject spit, target;
+	public float spitSpeed;
+
+	private Vector2 vectorToTarget;
+	private Vector3 rotation;
+	private float angle;
+	private Quaternion qt;
+	private float 
+		spitTimer = 0f,
+		SPIT_TIME_MAX = 2.0f;
+
+	public Spitting_Spider_Script() {
+		health = 10.0f;
+		damage = 2.0f;
+
+		name = "Spitting Spider";
+	}
+
+	// Use this for initialization
+	void Start () {
+		setSpriteRenderer (GetComponent<SpriteRenderer> ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isPaused()) {
+			vectorToTarget = target.transform.position - transform.position;
+			angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg) - 90;
+			qt = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.Slerp(transform.rotation, qt, Time.deltaTime * 5f);
+			flash ();
+			ShootSpits (playerIsClose());
+		} else {
+			// Do Nothing
+		}
+
+	}
+
+	void ShootSpits(bool shoot) {
+		spitTimer += Time.deltaTime;
+		if (shoot && spitTimer>SPIT_TIME_MAX) {
+			spitTimer = 0f;
+			GameObject spitClone = (GameObject)Instantiate (spit, transform.position, Quaternion.Euler (rotation));
+			spitClone.GetComponent<Rigidbody2D> ().AddForce (vectorToTarget*spitSpeed);
+			Debug.Log ("SPITTED !!");
+		}
+	}
+
+	bool playerIsClose() {
+		bool farAway = true;
+		if (vectorToTarget.x>-6 && vectorToTarget.x<6 && vectorToTarget.y>-6 && vectorToTarget.y<6) {
+			farAway = false;
+		}
+		return !farAway;
+	}
+}
